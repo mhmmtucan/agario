b2a6cc214ae0ffa3d5f4c2be18a20e7ed5c5df67
malkoch <hamitcan_malkoc@hotmail.com>
2018-02-16 09:28:43 +0300

    collecting data & creating network model
    now created a function to convert our collected data into an experience buffer
    
    trying to create a model with 3k data
    right now we have a problem with layer concatination

--- data_collector.py
+++ data_collector.py
@@ -1,3 +1,4 @@
+import cv2
 import time
 import pyautogui
 
@@ -9,18 +10,45 @@ from utils import InputCheck
 from utils import ExperienceBuffer
 from frame_processor import process
 
+def convert_data(areas, frames, mouses, spaces):
+    episode_buffer = ExperienceBuffer()
+
+    frame_num = len(frames)
+    look_up = 5
+
+    # we might want to remove last five frame when processing
+    # if we remove last five frame from the frames list we can have non-zero last areas
+    # if we dont we can remove the frames after the character died so we could get rid of menu frames
+    for i, frame in enumerate(frames):
+        past_areas = []
+        current_area = areas[i]
+        future_areas = []
+        current_frame = frames[i]
+        mouse = mouses[i]
+        space = spaces[i]
+
+        if i < look_up:
+            past_areas = [0] * (look_up - i) + areas[0 : i]
+        else:
+            past_areas = areas[i - look_up : i]
+
+        if i >= frame_num - look_up:
+            future_areas = areas[i + 1 : frame_num] + [0] * (i + look_up - frame_num + 1)
+        else:
+            future_areas = areas[i + 1 : i + 6]
+
+        episode_buffer.add(np.reshape(np.array([past_areas, current_area, future_areas, mouse, space, current_frame]), [1, 6]))
+
+    return episode_buffer
+
 if __name__ == '__main__':
     config = Config()
 
     paused = False
-    window_width, window_height = pyautogui.size()
     ic = InputCheck(config)
 
     max_buffer_size = 50000
     session_buffer = ExperienceBuffer(max_buffer_size)
-
-    filenames = ['first', 'second', 'third', 'fourth', 'fifth']
-    filename_index = 0
 
     sct = mss()
     
@@ -46,6 +74,10 @@ if __name__ == '__main__':
 
             frame, area, base_color = process(image, base_color, config)
 
+            #cv2.imshow('frame', frame)
+            #cv2.moveWindow('frame', 0, 0)
+            #cv2.waitKey(1)
+
             areas.append(area)
             frames.append(frame)
             mouses.append(ic.get_mouse_vector(pyautogui.position()))
@@ -68,50 +100,14 @@ if __name__ == '__main__':
 
             print('processing the user experience started')
             # create a function instead of this
-            frame_num = len(frames)
-            look_up = 5
-
-            # we might want to remove last five frame when processing
-            # if we remove last five frame from the frames list we can have non-zero last areas
-            # if we dont we can remove the frames after the character died so we could get rid of menu frames
-            for i, frame in enumerate(frames):
-                past_areas = []
-                area = areas[i]
-                future_areas = []
-                frame = frames[i]
-                mouse = mouses[i]
-                space = spaces[i]
-
-                if i < look_up:
-                    past_areas = [0] * (look_up - i) + areas[0 : i]
-                else:
-                    past_areas = areas[i - look_up : i]
-
-                if i >= frame_num - look_up:
-                    future_areas = areas[i + 1 : frame_num] + [0] * (i + look_up - frame_num + 1)
-                else:
-                    future_areas = areas[i + 1 : i + 6]
-
-
-                #print('frame number: {}, past_areas: {}'.format(i, past_areas))
-                #print('frame number: {}, area: {}'.format(i, area))
-                #print('frame number: {}, future_areas: {}'.format(i, future_areas))
-                #print('frame number: {}, frame: \n{}'.format(i, frame))
-                #print('frame number: {}, mouse: {}'.format(i, mouse))
-                #print('frame number: {}, space: {}'.format(i, space))
-                #print('')
-
-                # need to change the name of the file everytime or need to do load and save which will take more time
-                # used half of the screen as roi and resized to half when processing
-                # 75 frame took 20mb
-                session_buffer.add(np.reshape(np.array([past_areas, area, future_areas, mouse, space, frame]), [1, 6]))
-                session_buffer.save('training_data.npy')
-
-                if session_buffer.length == max_buffer_size:
-                    session_buffer = ExperienceBuffer(max_buffer_size)
-                    print('experience buffer is full')
+            session_buffer.add(convert_data(areas, frames, mouses, spaces).buffer)
+            session_buffer.save('training_data.npy')
 
             print('processing the user experience finnished')
+
+            if session_buffer.length == max_buffer_size:
+                session_buffer = ExperienceBuffer(max_buffer_size)
+                print('experience buffer is full')
 
         if 'C' in keys: # continue
             print('continue processing')
--- network.py
+++ network.py
@@ -1,35 +1,42 @@
 import tflearn
 
+import numpy as np
 import tensorflow as tf
+
+from utils import Config
 
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
 from tflearn.layers.normalization import local_response_normalization
 
-def CNN(input, config):
-    previous_area_input = input[0]
-    current_area_input = input[1]
-    future_area_input = input[2]
-    mouse_input = input[3]
-    space_input = input[4]
-    image_input = input[5]
+def customized_loss(out_true, out_pred, loss='euclidian'):
+    # euclidian distance loss
+    if loss=='euclidian':
+        val = tf.sqrt(tf.add(tf.square(out_pred - out_true), axis=-1))
+    return val
 
-    input_layer = input_data(shape=[None, config.sample_width, config.sample_height, config.samle_depth], name='main-input')
-    first_convolutional_layer = conv_2d(input_layer, 96, 11, strides=4, activation='relu')
+def CNN(config):
+    previous_area_input_layer = input_data(shape=[None, 5, 1, 1], name='prev')
+    current_area_input_layer = input_data(shape=[None, 1, 1, 1], name='current')
+    mouse_input_layer = input_data(shape=[None, 9, 1, 1], name='mouse')
+    space_input_layer = input_data(shape=[None, 1, 1, 1], name='space')
+    frame_input_layer = input_data(shape=[None, config.sample_width, config.sample_height, config.sample_depth], name='frame')
+
+    first_convolutional_layer = conv_2d(frame_input_layer, 96, 11, strides=4, activation='relu')
     first_pooling_layer = max_pool_2d(first_convolutional_layer, 3, strides=3)
     first_normalization_layer = local_response_normalization(first_pooling_layer)
 
     second_convolutional_layer = conv_2d(first_normalization_layer, 256, 11, strides=4, activation='relu')
     second_pooling_layer = max_pool_2d(second_convolutional_layer, 3, strides=3)
-    secon_normalization_layer = local_response_normalization(second_pooling_layer)
+    second_normalization_layer = local_response_normalization(second_pooling_layer)
 
-    third_convolutional_layer = conv_2d(secon_normalization_layer, 256, 11, strides=4, activation='relu')
+    third_convolutional_layer = conv_2d(second_normalization_layer, 256, 11, strides=4, activation='relu')
     third_pooling_layer = max_pool_2d(third_convolutional_layer, 3, strides=3)
     thid_normalization_layer = local_response_normalization(third_pooling_layer)
 
     first_part_output = tflearn.flatten(third_convolutional_layer)
-    merged_output = tf.concat([previous_area_input, current_area_input, future_area_input, mouse_input, space_input, first_part_output], 0) #future area will be output
+    merged_output = tf.concat([previous_area_input_layer, current_area_input_layer, mouse_input_layer, space_input_layer, first_part_output], 0)
 
     first_dense_layer = fully_connected(merged_output, 1024, activation='relu')
     first_dropout_layer = dropout(first_dense_layer, .5)
@@ -41,5 +48,42 @@ def CNN(input, config):
     third_dropout_layer = dropout(third_dense_layer, .5)
 
     final_layer = fully_connected(third_dropout_layer, config.actions, activation='softmax')
+    
+    # need to write our own loss function
+    regression_layer = regression(final_layer, optimizer='momentum', loss=customized_loss, learning_rate=1e-3, name='target')
 
-    return final_layer
+    model = tflearn.DNN(regression_layer, checkpoint_path='model', max_checkpoints=1, tensorboard_verbose=2, tensorboard_dir='log')
+    return model
+
+def train_model(model, input, test, config):
+    prev = np.array([i[0] for i in input]).reshape(-1, 5, 1, 1)
+    current = np.array([i[1] for i in input]).reshape(-1, 1, 1, 1)
+    future = np.array([i[2] for i in input]).reshape(-1, 5, 1, 1) # output
+    mouse = np.array([i[3] for i in input]).reshape(-1, 9, 1, 1)
+    space = np.array([i[4] for i in input]).reshape(-1, 1, 1, 1)
+    frame = np.array([i[5] for i in input]).reshape(-1, config.sample_width, config.sample_height, config.sample_depth)
+
+    test_prev = np.array([i[0] for i in test]).reshape(-1, 5, 1, 1)
+    test_current = np.array([i[1] for i in test]).reshape(-1, 1, 1, 1)
+    test_future = np.array([i[2] for i in test]).reshape(-1, 5, 1, 1) # output
+    test_mouse = np.array([i[3] for i in test]).reshape(-1, 9, 1, 1)
+    test_space = np.array([i[4] for i in test]).reshape(-1, 1, 1, 1)
+    test_frame = np.array([i[5] for i in test]).reshape(-1, config.sample_width, config.sample_height, config.sample_depth)
+    
+    model.fit({'prev': prev, 'current': current, 'mouse': mouse, 'space': space, 'frame': frame}, {'target': future},
+              n_epoch=8,
+              validation_set=({'prev': test_prev, 'current': test_current, 'mouse': test_mouse, 'space': test_space, 'frame': test_frame}, {'target': test_future}),
+              snapshot_step=250,
+              show_metric=True,
+              run_id='agario-v0.model')
+    model.save('agario-v0.model')
+
+if __name__ == '__main__':
+    config = Config()
+    model = CNN(config)
+
+    train_data = np.load('training_data.npy')
+    input = train_data[:-500]
+    test = train_data[-500:]
+
+    train_model(model, input, test, config)
