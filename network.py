b5ecc057e6c97e6c0a33f4b953666ad3208c0cc1
malkoch <hamitcan_malkoc@hotmail.com>
2018-02-17 14:34:46 +0300

    adding extra info
    now we are adding extra informations to the network but it does not seem fine, it can be done better. we will try to fidn better solutions for that

--- network.py
+++ network.py
@@ -13,27 +13,34 @@ from tflearn.layers.normalization import local_response_normalization
 
 # tensorboard --logdir=foo:C:/Users/H/Desktop/ai-gaming/log
 
-def alexnet(config):
-    network = input_data(shape=[None, config.sample_height, config.sample_width, config.sample_depth], name='input')
+def convnet(config):
+    network = input_data(shape=[None, config.sample_height, config.sample_width, config.sample_depth], name='input') # h = 90, w = 150, d = 1
 
-    side_network1 = input_data(shape=[None, 5], name='prev') # previous areas
-    side_network2 = input_data(shape=[None, 1], name='curr') # current area
-    side_network3 = input_data(shape=[None, 9], name='mous') # mouse position
-    side_network4 = input_data(shape=[None, 1], name='spac') # space hit
+    side_network1 = input_data(shape=[None, 12, 19, 5], name='prev') # previous areas
+    side_network2 = input_data(shape=[None, 12, 19, 1], name='curr') # current area
+    side_network3 = input_data(shape=[None, 12, 19, 9], name='mous') # mouse position
+    side_network4 = input_data(shape=[None, 12, 19, 1], name='spac') # space hit
 
-    network = conv_2d(network, 64, 11, strides=4, activation='relu')
-    network = max_pool_2d(network, 3, strides=2)
+    # output_width = ceil(width / stride_width)
+    # output_height = ceil(height / stride_height)
+    
+    network = conv_2d(network, 64, 7, activation='relu') # h = 90, w = 150, d = 64
+    network = max_pool_2d(network, 3, strides=2) # h = 45, w = 75, d = 64
+    network = local_response_normalization(network)
+    
+    network = conv_2d(network, 32, 5, activation='relu') # h = 45, w = 75, d = 32
+    network = max_pool_2d(network, 3, strides=2) # h = 23, w = 38, d = 32
     network = local_response_normalization(network)
 
-    network = conv_2d(network, 32, 7, activation='relu')
-    network = max_pool_2d(network, 3, strides=2)
+    network = conv_2d(network, 16, 3, activation='relu') # h = 23, w = 38, d = 16
+    network = max_pool_2d(network, 3, strides=2) # h = 12, w = 19, d = 16
     network = local_response_normalization(network)
 
-    network = conv_2d(network, 16, 5, activation='relu')
-    network = conv_2d(network, 16, 5, activation='relu')
-    network = conv_2d(network, 16, 5, activation='relu')
-    network = max_pool_2d(network, 3, strides=2)
-    network = local_response_normalization(network)
+    # this kind of concatination does not look good
+    # try to change it
+    # try to find better options
+    # maybe they can be used as weight or bias
+    network = tf.concat([network, side_network1, side_network2, side_network3, side_network4], axis=3)
 
     network = fully_connected(network, 100, activation='tanh')
     network = dropout(network, 0.5)
@@ -45,26 +52,35 @@ def alexnet(config):
     network = fully_connected(network, 5, activation='softmax') # frame -> future area
 
     # need to write custom loss function, categorical crossentropy is good for binary classification
-    network = regression(network, optimizer='momentum', loss='categorical_crossentropy', learning_rate=1e-3, name='targets')
+    # mean_squared_error or mean_pairwise_squared_error
+    network = regression(network, optimizer='adam', loss=tf.losses.mean_squared_error, learning_rate=1e-3, name='targets')
 
     model = tflearn.DNN(network, checkpoint_path='model_alexnet', max_checkpoints=1, tensorboard_verbose=2, tensorboard_dir='log')
 
     return model
 
-def train_alexnet(model, input, test, config):
+def train_convnet(model, input, test, config):
     X = np.array([i[5] for i in input]).reshape(-1, config.sample_height, config.sample_width, config.sample_depth) # frame
-    X1 = [i[0] for i in input] # previous areas
-    X2 = [i[1] for i in input] # current area
-    X3 = [i[3] for i in input] # mouse position
-    X4 = [i[4] for i in input] # space hit
+    X1 = np.array([np.tile(i[0], (12, 19)) for i in input]).reshape(-1, 12, 19, 5) # previous areas
+    X2 = np.array([np.tile(i[1], (12, 19)) for i in input]).reshape(-1, 12, 19, 1) # current area
+    X3 = np.array([np.tile(i[3], (12, 19)) for i in input]).reshape(-1, 12, 19, 9) # mouse position
+    X4 = np.array([np.tile(i[4], (12, 19)) for i in input]).reshape(-1, 12, 19, 1) # space hit
+    #X1 = [i[0] for i in input] # previous areas
+    #X2 = [i[1] for i in input] # current area
+    #X3 = [i[3] for i in input] # mouse position
+    #X4 = [i[4] for i in input] # space hit
 
     Y = [i[2] for i in input] # future areas
 
     test_X = np.array([i[5] for i in test]).reshape(-1, config.sample_height, config.sample_width, config.sample_depth)
-    test_X1 = [i[0] for i in test] # previous areas
-    test_X2 = [i[1] for i in test] # current area
-    test_X3 = [i[3] for i in test] # mouse position
-    test_X4 = [i[4] for i in test] # space hit
+    test_X1 = np.array([np.tile(i[0], (12, 19)) for i in test]).reshape(-1, 12, 19, 5) # previous areas
+    test_X2 = np.array([np.tile(i[1], (12, 19)) for i in test]).reshape(-1, 12, 19, 1) # current area
+    test_X3 = np.array([np.tile(i[3], (12, 19)) for i in test]).reshape(-1, 12, 19, 9) # mouse position
+    test_X4 = np.array([np.tile(i[4], (12, 19)) for i in test]).reshape(-1, 12, 19, 1) # space hit
+    #test_X1 = [i[0] for i in test] # previous areas
+    #test_X2 = [i[1] for i in test] # current area
+    #test_X3 = [i[3] for i in test] # mouse position
+    #test_X4 = [i[4] for i in test] # space hit
 
     test_Y = [i[2] for i in test] # future areas
 
@@ -104,8 +120,8 @@ if __name__ == '__main__':
     tf.reset_default_graph()
     tf.get_default_graph()
 
-    model = alexnet(config)
+    model = convnet(config)
     print('created the model')
 
-    train_alexnet(model, input, test, config)
+    train_convnet(model, input, test, config)
     print('trained the model')
