1bdd9ff06b438688298345e7fc251865e2ffde6a
malkoch <hamitcan_malkoc@hotmail.com>
2018-02-17 13:06:42 +0300

    network update
    removed unnecessary network, no need to get confused
    
    added four side network input layer we can use them to merge, concat with the main network or we can use them as weight or bias to get the output

--- check_data.py
+++ check_data.py
@@ -3,26 +3,62 @@ import pandas as pd
 
 from collections import Counter
 
-data = np.load('training_data.npy')
+def print_data(data):
+    df = pd.DataFrame(data)
 
-df = pd.DataFrame(data)
+    print('len')
+    print(len(data)) # printing how much data we got so far
+    print('')
 
-print('len')
-print(len(data)) # printing how much data we got so far
-print('')
+    print('first ten data')
+    print(df.head(10)) # printing the first ten data
+    print('')
 
-print('first ten data')
-print(df.head(10)) # printing the first ten data
-print('')
+    print('last ten data')
+    print(df.tail(10)) # printing the last ten data
+    print('')
 
-print('last ten data')
-print(df.tail(10)) # printing the last ten data
-print('')
+    print('mouse position counter')
+    print(Counter(df[3].apply(str))) # printing the count of the mouse position -> 3 = mouse position
+    print('')
 
-print('mouse position counter')
-print(Counter(df[3].apply(str))) # printing the count of the mouse position -> 3 = mouse position
-print('')
+    print('space key counter')
+    print(Counter(df[4].apply(str))) # printing the count of the space key strokes -> 4 = space key
+    print('')
 
-print('space key counter')
-print(Counter(df[4].apply(str))) # printing the count of the space key strokes -> 4 = space key
-print('')
+def print_size(data):
+    prevs = data[:, 0]
+    currs = data[:, 1]
+    futus = data[:, 2]
+    mouss = data[:, 3]
+    spacs = data[:, 4]
+    frams = data[:, 5]
+
+    print('prev size: {}, cur size: {}, fut size: {}, mous size: {}, space size: {}, frame size: {}'.format(prevs.shape, currs.shape, futus.shape, mouss.shape, spacs.shape, frams.shape))
+    print('prev dtype: {}, cur dtype: {}, fut dtype: {}, mous dtype: {}, space dtype: {}, frame dtype: {}'.format(prevs.dtype, currs.dtype, futus.dtype, mouss.dtype, spacs.dtype, frams.dtype))
+    print('')
+    print('prev size: {}, cur size: {}, fut size: {}, mous size: {}, space size: {}, frame size: {}'.format(prevs[0].shape, currs[0].shape, futus[0].shape, mouss[0].shape, spacs[0].shape, frams[0].shape))
+    print('prev dtype: {}, cur dtype: {}, fut dtype: {}, mous dtype: {}, space dtype: {}, frame dtype: {}'.format(prevs[0].dtype, currs[0].dtype, futus[0].dtype, mouss[0].dtype, spacs[0].dtype, frams[0].dtype))
+
+def print_play(data):
+    prevs = np.array([i[0] for i in data], dtype=np.float32).reshape((-1, 5))
+    currs = np.array([i[1] for i in data], dtype=np.float32).reshape((-1, 1))
+    futus = np.array([i[2] for i in data], dtype=np.float32).reshape((-1, 5))
+    mouss = np.array([i[3] for i in data], dtype=np.float32).reshape((-1, 9))
+    spacs = np.array([i[4] for i in data], dtype=np.float32).reshape((-1, 1))
+    frams = np.array([i[5] for i in data], dtype=np.float32).reshape((-1, 90, 150, 1))
+    
+    print('prev size: {}, cur size: {}, fut size: {}, mous size: {}, space size: {}, frame size: {}'.format(prevs.shape, currs.shape, futus.shape, mouss.shape, spacs.shape, frams.shape))
+    print('prev dtype: {}, cur dtype: {}, fut dtype: {}, mous dtype: {}, space dtype: {}, frame dtype: {}'.format(prevs.dtype, currs.dtype, futus.dtype, mouss.dtype, spacs.dtype, frams.dtype))
+    print('')
+    print('prev size: {}, cur size: {}, fut size: {}, mous size: {}, space size: {}, frame size: {}'.format(prevs[0].shape, currs[0].shape, futus[0].shape, mouss[0].shape, spacs[0].shape, frams[0].shape))
+    print('prev dtype: {}, cur dtype: {}, fut dtype: {}, mous dtype: {}, space dtype: {}, frame dtype: {}'.format(prevs[0].dtype, currs[0].dtype, futus[0].dtype, mouss[0].dtype, spacs[0].dtype, frams[0].dtype))
+
+if __name__ == '__main__':
+    data = np.load('training_data.npy')
+
+    #print_data(data)
+    #print('')
+    #print_size(data)
+    #print('')
+    print_play(data)
--- network.py
+++ network.py
@@ -1,3 +1,4 @@
+import cv2
 import tflearn
 
 import numpy as np
@@ -12,207 +13,80 @@ from tflearn.layers.normalization import local_response_normalization
 
 # tensorboard --logdir=foo:C:/Users/H/Desktop/ai-gaming/log
 
-def alexnet2(config):
-    network = input_data(shape=[None, config.sample_width, config.sample_height, config.sample_depth], name='input0') # frame
+def alexnet(config):
+    network = input_data(shape=[None, config.sample_height, config.sample_width, config.sample_depth], name='input')
 
-    side_network1 = input_data(shape=[None, 5], name='input1') # past
-    side_network2 = input_data(shape=[None, 1], name='input2') # current
-    side_network3 = input_data(shape=[None, 9], name='input3') # mouse
-    side_network4 = input_data(shape=[None, 1], name='input4') # space
+    side_network1 = input_data(shape=[None, 5], name='prev') # previous areas
+    side_network2 = input_data(shape=[None, 1], name='curr') # current area
+    side_network3 = input_data(shape=[None, 9], name='mous') # mouse position
+    side_network4 = input_data(shape=[None, 1], name='spac') # space hit
 
+    network = conv_2d(network, 64, 11, strides=4, activation='relu')
     network = max_pool_2d(network, 3, strides=2)
     network = local_response_normalization(network)
-    network = conv_2d(network, 256, 5, activation='relu')
+
+    network = conv_2d(network, 32, 7, activation='relu')
     network = max_pool_2d(network, 3, strides=2)
     network = local_response_normalization(network)
-    network = conv_2d(network, 384, 3, activation='relu')
-    network = conv_2d(network, 384, 3, activation='relu')
-    network = conv_2d(network, 256, 3, activation='relu')
+
+    network = conv_2d(network, 16, 5, activation='relu')
+    network = conv_2d(network, 16, 5, activation='relu')
+    network = conv_2d(network, 16, 5, activation='relu')
     network = max_pool_2d(network, 3, strides=2)
     network = local_response_normalization(network)
-    network = fully_connected(network, 4096, activation='tanh')
+
+    network = fully_connected(network, 100, activation='tanh')
     network = dropout(network, 0.5)
-    network = fully_connected(network, 4096, activation='tanh')
+
+    network = fully_connected(network, 50, activation='tanh')
     network = dropout(network, 0.5)
-    network = fully_connected(network, 5, activation='softmax')
 
-    #final_layer_previous_area_part = fully_connected(side_network1, 5, activation='softmax')
-    #final_layer_current_area_part = fully_connected(side_network2, 5, activation='softmax')
-    #final_layer_mouse_part = fully_connected(side_network3, 5, activation='softmax')
-    #final_layer_space_part = fully_connected(side_network4, 5, activation='softmax')
+    #network = fully_connected(network, config.actions, activation='softmax') # frame -> mouse
+    network = fully_connected(network, 5, activation='softmax') # frame -> future area
 
-    #network = tf.concat([final_layer_previous_area_part, final_layer_current_area_part, final_layer_mouse_part, final_layer_space_part, network], 0)
-    #network = dropout(network, .2)
-    #network = fully_connected(network, 5, activation='softmax')
-
+    # need to write custom loss function, categorical crossentropy is good for binary classification
     network = regression(network, optimizer='momentum', loss='categorical_crossentropy', learning_rate=1e-3, name='targets')
 
-    model = tflearn.DNN(network, checkpoint_path='model_alexnet2', max_checkpoints=1, tensorboard_verbose=2, tensorboard_dir='log')
-
-    return model
-
-def train_alexnet2(model, input, test, config):
-    X0 = np.array([i[5] for i in input]).reshape(-1, config.sample_width, config.sample_height, config.sample_depth)
-    X1 = np.array([i[0] for i in input])
-    X2 = np.array([i[1] for i in input])
-    X3 = np.array([i[3] for i in input])
-    X4 = np.array([i[4] for i in input])
-
-    Y = [i[2] for i in input]
-
-    test_x0 = np.array([i[5] for i in test]).reshape(-1, config.sample_width, config.sample_height, config.sample_depth)
-    test_x1 = np.array([i[0] for i in test])
-    test_x2 = np.array([i[1] for i in test])
-    test_x3 = np.array([i[3] for i in test])
-    test_x4 = np.array([i[4] for i in test])
-    test_y = [i[2] for i in test]
-
-    model.fit({'input0': X0,
-               'input1': X1,
-               'input2': X2,
-               'input3': X3,
-               'input4': X4}, {'targets': Y},
-              n_epoch=10,
-              validation_set=({'input0': test_x0,
-                               'input1': test_x1,
-                               'input2': test_x2,
-                               'input3': test_x3,
-                               'input4': test_x4},
-                              {'targets': test_y}), 
-    snapshot_step=50, show_metric=True, run_id='agario-alexnet2.model')
-    
-    model.save('agario-alexnet2.model')
-
-def alexnet(config):
-    network = conv_2d(network, 96, 11, strides=4, activation='relu')
-    network = max_pool_2d(network, 3, strides=2)
-    network = local_response_normalization(network)
-    network = conv_2d(network, 256, 5, activation='relu')
-    network = max_pool_2d(network, 3, strides=2)
-    network = local_response_normalization(network)
-    network = conv_2d(network, 384, 3, activation='relu')
-    network = conv_2d(network, 384, 3, activation='relu')
-    network = conv_2d(network, 256, 3, activation='relu')
-    network = max_pool_2d(network, 3, strides=2)
-    network = local_response_normalization(network)
-    network = fully_connected(network, 4096, activation='tanh')
-    network = dropout(network, 0.5)
-    network = fully_connected(network, 4096, activation='tanh')
-    network = dropout(network, 0.5)
-    network = fully_connected(network, config.actions, activation='softmax')
-    network = regression(network, optimizer='momentum',
-                         loss='categorical_crossentropy',
-                         learning_rate=1e-3, name='targets')
-
-    model = tflearn.DNN(network, checkpoint_path='model_alexnet',
-                        max_checkpoints=1, tensorboard_verbose=2, tensorboard_dir='log')
+    model = tflearn.DNN(network, checkpoint_path='model_alexnet', max_checkpoints=1, tensorboard_verbose=2, tensorboard_dir='log')
 
     return model
 
 def train_alexnet(model, input, test, config):
-    X = np.array([i[5] for i in input]).reshape(-1, config.sample_width, config.sample_height, config.sample_depth)
-    Y = [i[3] for i in input]
+    X = np.array([i[5] for i in input]).reshape(-1, config.sample_height, config.sample_width, config.sample_depth) # frame
+    X1 = [i[0] for i in input] # previous areas
+    X2 = [i[1] for i in input] # current area
+    X3 = [i[3] for i in input] # mouse position
+    X4 = [i[4] for i in input] # space hit
 
-    test_x = np.array([i[5] for i in test]).reshape(-1, config.sample_width, config.sample_height, config.sample_depth)
-    test_y = [i[3] for i in test]
+    Y = [i[2] for i in input] # future areas
 
-    model.fit({'input': X}, {'targets': Y}, n_epoch=8, validation_set=({'input': test_x}, {'targets': test_y}), 
-    snapshot_step=500, show_metric=True, run_id='agario-alexnet.model')
+    test_X = np.array([i[5] for i in test]).reshape(-1, config.sample_height, config.sample_width, config.sample_depth)
+    test_X1 = [i[0] for i in test] # previous areas
+    test_X2 = [i[1] for i in test] # current area
+    test_X3 = [i[3] for i in test] # mouse position
+    test_X4 = [i[4] for i in test] # space hit
+
+    test_Y = [i[2] for i in test] # future areas
+
+    model.fit([X, X1, X2, X3, X4], Y, n_epoch=8, validation_set=([test_X, test_X1, test_X2, test_X3, test_X4], test_Y), snapshot_step=500, show_metric=True, run_id='agario-alexnet.model')
     
     model.save('agario-alexnet.model')
 
-def customized_loss(y_pred, y_true, loss='euclidian'):
-    # euclidian distance loss
-    if loss=='euclidian':
-        val = tf.sqrt(tf.reduce_sum(tf.square(y_pred - y_true), axis=-1))
-    return val
+def show_images(input, test, config):
+    input_frames = np.array([i[5] for i in input]).reshape(-1, config.sample_height, config.sample_width, config.sample_depth)
+    test_frames = np.array([i[5] for i in test]).reshape(-1, config.sample_height, config.sample_width, config.sample_depth)
 
-def CNN(input, config):
-    frames = input[:, 5]
-    #previous_area_input_layer = tf.placeholder(dtype=tf.float32, shape=(None, 1, 5, 1), name='prev')
-    #current_area_input_layer = tf.placeholder(dtype=tf.float32, shape=(None, 1, 1, 1), name='current')
-    #future_area_input_layer = tf.placeholder(dtype=tf.float32, shape=(None, 1, 5, 1), name='future')
-    #mouse_input_layer = tf.placeholder(dtype=tf.float32, shape=(None, 1, 9, 1), name='mouse')
-    #space_input_layer = tf.placeholder(dtype=tf.float32, shape=(None, 1, 1, 1), name='space')
-    frame_input_layer = tf.reshape(frames, [-1, config.sample_width, config.sample_height, config.sample_depth], name='frame')
+    for frame in input_frames:
+        cv2.imshow('input', frame)
+        cv2.waitKey(1)
 
-    first_convolutional_layer = conv_2d(frame_input_layer, 96, 11, strides=4, activation='relu')
-    first_pooling_layer = max_pool_2d(first_convolutional_layer, 3, strides=3)
-    first_normalization_layer = local_response_normalization(first_pooling_layer)
+    cv2.destroyWindow('input')
 
-    second_convolutional_layer = conv_2d(first_normalization_layer, 256, 11, strides=4, activation='relu')
-    second_pooling_layer = max_pool_2d(second_convolutional_layer, 3, strides=3)
-    second_normalization_layer = local_response_normalization(second_pooling_layer)
+    for frame in test_frames:
+        cv2.imshow('test', frame)
+        cv2.waitKey(1)
 
-    third_convolutional_layer = conv_2d(second_normalization_layer, 256, 11, strides=4, activation='relu')
-    third_pooling_layer = max_pool_2d(third_convolutional_layer, 3, strides=3)
-    third_normalization_layer = local_response_normalization(third_pooling_layer)
-
-    '''first_part_output = tflearn.flatten(third_convolutional_layer)
-
-    merged_output = tf.concat([previous_area_input_layer, current_area_input_layer, mouse_input_layer, space_input_layer, first_part_output], 0)
-
-    first_dense_layer = fully_connected(merged_output, 1024, activation='relu')
-    first_dropout_layer = dropout(first_dense_layer, .5)
-        
-    second_dense_layer = fully_connected(first_dropout_layer, 100, activation='relu')
-    second_dropout_layer = dropout(second_dense_layer, .5)
-
-    third_dense_layer = fully_connected(second_dropout_layer, 50, activation='relu')
-    third_dropout_layer = dropout(third_dense_layer, .5)
-
-    final_layer = fully_connected(third_dropout_layer, config.actions, activation='softmax')'''
-
-    first_dense_layer = fully_connected(third_normalization_layer, 1024, activation='relu')
-    first_dropout_layer = dropout(first_dense_layer, .5)
-
-    second_dense_layer = fully_connected(first_dropout_layer, 100, activation='relu')
-    second_dropout_layer = dropout(second_dense_layer, .5)
-
-    third_dense_layer = fully_connected(second_dropout_layer, 50, activation='relu')
-    third_dropout_layer = dropout(third_dense_layer, .5)
-
-    #final_layer_first_part = fully_connected(third_dropout_layer, config.actions, activation='softmax')
-    #final_layer_previous_area_part = fully_connected(previous_area_input_layer, config.actions, activation='softmax')
-    #final_layer_current_area_part = fully_connected(current_area_input_layer, config.actions, activation='softmax')
-    #final_layer_mouse_part = fully_connected(mouse_input_layer, config.actions, activation='softmax')
-    #final_layer_space_part = fully_connected(space_input_layer, config.actions, activation='softmax')
-    
-    #final_layer = tf.concat([final_layer_previous_area_part, final_layer_current_area_part, final_layer_mouse_part, final_layer_space_part, final_layer_first_part], 0)
-
-    # need to write our own loss function
-    regression_layer = regression(third_dropout_layer, optimizer='momentum', loss=customized_loss, learning_rate=1e-3, name='target')
-    #regression_layer = regression(final_layer, optimizer='momentum', loss=customized_loss, learning_rate=1e-3, name='target')
-
-    model = tflearn.DNN(regression_layer, checkpoint_path='model', max_checkpoints=1, tensorboard_verbose=2, tensorboard_dir='log')
-    return model
-
-def train_model(model, input, test, config):
-    # need to think about sizes of the every layer
-    #prev = np.array([i[0] for i in input])
-    #current = np.array([i[1] for i in input])
-    future = np.array([i[2] for i in input])
-    #mouse = np.array([i[3] for i in input])
-    #space = np.array([i[4] for i in input])
-    frame = np.array([i[5] for i in input])
-    
-    #test_prev = np.array([i[0] for i in test])
-    #test_current = np.array([i[1] for i in test])
-    test_future = np.array([i[2] for i in test])
-    #test_mouse = np.array([i[3] for i in test])
-    #test_space = np.array([i[4] for i in test])
-    test_frame = np.array([i[5] for i in test])
-
-    model.fit({'frame': frame},
-              {'target': future},
-              n_epoch=8,
-              validation_set=({'frame': test_frame},
-                              {'target': test_future}),
-              snapshot_step=250,
-              show_metric=True,
-              run_id='agario-v0.model')
-    model.save('agario-v0.model')
-
+    cv2.destroyWindow('test')
 
 if __name__ == '__main__':
     config = Config()
@@ -224,12 +98,14 @@ if __name__ == '__main__':
     test = train_data[-test_data_len:]
     print('got the data')
 
-    #model = CNN(input, config)
-    #model = alexnet(config)
-    model = alexnet2(config)
+    #show_images(input, test, config)
+
+    tf.get_default_graph()
+    tf.reset_default_graph()
+    tf.get_default_graph()
+
+    model = alexnet(config)
     print('created the model')
 
-    #train_model(model, input, test, config)
-    #train_alexnet(model, input, test, config)
-    train_alexnet2(model, input, test, config)
+    train_alexnet(model, input, test, config)
     print('trained the model')
