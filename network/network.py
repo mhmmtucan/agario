13c820f2f3936da2ca4cd0566085e67e51e63fad
Hamitcan MALKOÇ <hamitcan_malkoc@hotmail.com>
2018-02-19 12:02:03 +0200

    removing unnecessary network
    shuffling the data before giving it to the network
    removing different types of inputs, for now we are giving a main input that is the frame and side input which is the combination of extra informations
    
    more convolutional layer is added to the network but for now they are commented out since we need a speed right now

--- network/network.py
+++ network/network.py
@@ -5,12 +5,13 @@ import numpy as np
 import tensorflow as tf
 
 from utils import Config
+from random import shuffle
 
 from tflearn.layers.conv import conv_2d
 from tflearn.layers.conv import max_pool_2d
 
-from tflearn.layers.core import input_data
 from tflearn.layers.core import dropout
+from tflearn.layers.core import input_data
 from tflearn.layers.core import fully_connected
 
 from tflearn.layers.estimator import regression
@@ -24,46 +25,32 @@ def convnet(config):
     network = input_data(shape=[None, config.sample_height, config.sample_width, config.sample_depth], name='input') # h = 90, w = 150, d = 1
 
     side_network = input_data(shape=[None, 16], name='extra-input')
-    side_network = fully_connected(side_network, 16, activation='relu')
+    side_network = fully_connected(side_network, 64, activation='relu')
     side_network = tf.tile(side_network, [1, 3375])
-    side_network = tf.reshape(side_network, shape=[-1, 45, 75, 16])
+    side_network = tf.reshape(side_network, shape=[-1, 45, 75, 64])
     # not sure if we are doing this right
-
-    #side_network1 = input_data(shape=[None, 5], name='prev') # previous areas
-    #side_network2 = input_data(shape=[None, 1], name='curr') # current area
-    #side_network3 = input_data(shape=[None, 9], name='mous') # mouse position
-    #side_network4 = input_data(shape=[None, 1], name='spac') # space hit
 
     # output_width = ceil(width / stride_width)
     # output_height = ceil(height / stride_height)
     
     network = conv_2d(network, 64, 5, activation='relu') # h = 90, w = 150, d = 64
     network = batch_normalization(network)
-    network = conv_2d(network, 64, 5, activation='relu') # h = 90, w = 150, d = 64
-    network = batch_normalization(network)
+    #network = conv_2d(network, 64, 5, activation='relu') # h = 90, w = 150, d = 64
+    #network = batch_normalization(network)
     network = max_pool_2d(network, 3, strides=2) # h = 45, w = 75, d = 64 
-    #network = tf.add(network, side_network);
+    network = tf.add(network, side_network);
 
     network = conv_2d(network, 64, 3, activation='relu') # h = 45, w = 75, d = 64
     network = batch_normalization(network)
-    network = conv_2d(network, 64, 3, activation='relu') # h = 45, w = 75, d = 64
-    network = batch_normalization(network)
+    #network = conv_2d(network, 64, 3, activation='relu') # h = 45, w = 75, d = 64
+    #network = batch_normalization(network)
+    #network = conv_2d(network, 64, 3, activation='relu') # h = 45, w = 75, d = 64
+    #network = batch_normalization(network)
     network = max_pool_2d(network, 3, strides=2) # h = 23, w = 38, d = 64
 
     network = conv_2d(network, 64, 3, activation='relu') # h = 23, w = 38, d = 64
     network = batch_normalization(network)
-    network = conv_2d(network, 64, 3, activation='relu') # h = 23, w = 38, d = 64
-    network = batch_normalization(network)
     network = max_pool_2d(network, 3, strides=2) # h = 12, w = 19, d = 64
-
-    # this kind of concatination does not look good
-    # try to change it
-    # try to find better options
-    # maybe they can be used as weight or bias
-    # did not liked this approach, look into convolutional layers weight and biases to try to use them as weight and bias
-    # or look at the multi layer perceptron might be good idea to used those
-    #network = tf.concat([network, side_network1, side_network2, side_network3, side_network4], axis=3)
-    # since we are doing this way the loss and accuracy will not work properly
 
     network = fully_connected(network, 100, activation='relu')
     network = dropout(network, 0.5)
@@ -71,11 +58,13 @@ def convnet(config):
     network = fully_connected(network, 50, activation='relu')
     network = dropout(network, 0.5)
 
-    #network = fully_connected(network, config.actions, activation='softmax') # frame -> mouse
     network = fully_connected(network, 5, activation='relu') # frame -> future area
 
     # need to write custom loss function, categorical crossentropy is good for binary classification
     # mean_squared_error or mean_pairwise_squared_error
+    # think about logistic regression, linear regression
+    # logistic regression -> class prediction
+    # linear regression -> scalar prediction
     network = regression(network, optimizer='adam', loss=tf.losses.mean_squared_error, learning_rate=1e-3, name='targets')
 
     model = tflearn.DNN(network, checkpoint_path='model_convnet', max_checkpoints=1, tensorboard_verbose=2, tensorboard_dir='log')
@@ -84,10 +73,6 @@ def convnet(config):
 
 def train_convnet(model, input, test, config):
     X = np.array([i[5] for i in input]).reshape(-1, config.sample_height, config.sample_width, config.sample_depth) # frame
-    #X1 = np.array([np.tile(i[0], (12, 19)) for i in input]).reshape(-1, 12, 19, 5) # previous areas
-    #X2 = np.array([np.tile(i[1], (12, 19)) for i in input]).reshape(-1, 12, 19, 1) # current area
-    #X3 = np.array([np.tile(i[3], (12, 19)) for i in input]).reshape(-1, 12, 19, 9) # mouse position
-    #X4 = np.array([np.tile(i[4], (12, 19)) for i in input]).reshape(-1, 12, 19, 1) # space hit
     X1 = [i[0] for i in input] # previous areas
     X2 = [i[1] for i in input] # current area
     X3 = [i[3] for i in input] # mouse position
@@ -97,10 +82,6 @@ def train_convnet(model, input, test, config):
     Y = [i[2] for i in input] # future areas
 
     test_X = np.array([i[5] for i in test]).reshape(-1, config.sample_height, config.sample_width, config.sample_depth)
-    #test_X1 = np.array([np.tile(i[0], (12, 19)) for i in test]).reshape(-1, 12, 19, 5) # previous areas
-    #test_X2 = np.array([np.tile(i[1], (12, 19)) for i in test]).reshape(-1, 12, 19, 1) # current area
-    #test_X3 = np.array([np.tile(i[3], (12, 19)) for i in test]).reshape(-1, 12, 19, 9) # mouse position
-    #test_X4 = np.array([np.tile(i[4], (12, 19)) for i in test]).reshape(-1, 12, 19, 1) # space hit
     test_X1 = [i[0] for i in test] # previous areas
     test_X2 = [i[1] for i in test] # current area
     test_X3 = [i[3] for i in test] # mouse position
@@ -109,9 +90,7 @@ def train_convnet(model, input, test, config):
 
     test_Y = [i[2] for i in test] # future areas
 
-    # start using less epoch since we are going to use a lot of data
     model.fit([X, XSide], Y, n_epoch=6, validation_set=([test_X, test_XSide], test_Y), snapshot_step=500, show_metric=True, run_id='agario-convnet.model')
-    #model.fit([X, X1, X2, X3, X4], Y, n_epoch=1, validation_set=([test_X, test_X1, test_X2, test_X3, test_X4], test_Y), snapshot_step=500, show_metric=True, run_id='agario-convnet.model')
     
     model.save('agario-convnet.model')
 
@@ -137,15 +116,14 @@ def create_model(filename):
     train_data = np.load(filename)
     test_data_len = len(train_data) // 10
     #print(test_data_len)
+
+    shuffle(train_data)
+
     input = train_data[:-test_data_len]
     test = train_data[-test_data_len:]
     print('got the data')
 
     #show_images(input, test, config)
-
-    tf.get_default_graph()
-    tf.reset_default_graph()
-    tf.get_default_graph()
 
     model = convnet(config)
     print('created the model')
